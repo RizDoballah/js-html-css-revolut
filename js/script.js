fa5e4e46e36ac72887c71c187052d27c0e748101
Ghezlene Doballah <ghyzounette@gmail.com>
2020-01-20 18:10:19 +0100

    final commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(
   function() {
     $('.parent-dropdown > a').mouseover(
+    function() {
+      $(this).next('.dropdown').toggleClass('active');
+      }
+    );
+    $('.parent-dropdown > a').click(
       function() {
         $(this).next('.dropdown').toggleClass('active');
       }
